c6a4c86429109d1f30961c0ae4db5722d7524ae5
bryu0116 <69437446+bryu0116@users.noreply.github.com>
2020-10-28 00:49:55 -0400

    another commit

--- index.js
+++ index.js
@@ -1,16 +1,85 @@
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project? "
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter a Project Description: "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Enter a Project Installation: "
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about the Project?: "
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose a license for the project: ",
+        choices: ['GPL 3.0', 'ISC', 'BSD 3', 'GNU v3', 'Apache 2.0', 'MIT License', 'None']
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Who is/are the contributor(s) of the Project?: "
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Enter any test instructions: "
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Enter any questions or proposals for future work: "
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub username: ",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email address: ",
+    },
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
 // function to initialize program
-function init() {
-
+const promptUser = () => {
+    return inquirer
+        .prompt(questions);
 }
+
+async function init() {
+    try {
+      const answers = await promptUser();
+  
+      const generateReadMeContent = generateMarkdown(answers);
+  
+      await writeFileAsync('README.md', generateReadMeContent);
+  
+      console.log("Successfully wrote to README.md");
+    } catch(error) {
+      console.log(error);
+    }
+  }
 
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,36 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+function generateMarkdown(answers) {
+  return `
+<h1 align="center">${answers.title}</h1>
+ ${answers.description}
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Description
+## Installation
+${answers.installation}
+## Usage
+${answers.usage}
+## License
+${answers.license}
+<br />
+This application is covered by the ${answers.license} license. 
+## Contributing
+${answers.contributing}
+## Tests
+${answers.tests}
+## Questions
+${answers.questions}<br />
+<br />
+GitHub: ${answers.github}<br /><br />
+Email: ${answers.email}<br /><br />
+Copyright 2020 &copy;`;
 }
 
 module.exports = generateMarkdown;
